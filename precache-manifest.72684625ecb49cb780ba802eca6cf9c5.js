89ff339314951a3a403a06d6a0f4cc408907d8bd
Zhenyu Ye <zye@adaptivebiotech.com>
2021-07-19 12:01:54 -0700

    Updates

---
+++ precache-manifest.72684625ecb49cb780ba802eca6cf9c5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33082807cc4b8df329355babb71e90f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fd29af6a569b7309482",
+    "url": "/static/css/main.b42acfb8.chunk.css"
+  },
+  {
+    "revision": "f7934700bc67537422df",
+    "url": "/static/js/2.2b477505.chunk.js"
+  },
+  {
+    "revision": "0efabacf1d5a43cdae1da6fa187843ce",
+    "url": "/static/js/2.2b477505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b72c138632cda820e7",
+    "url": "/static/js/3.b218e390.chunk.js"
+  },
+  {
+    "revision": "6fd29af6a569b7309482",
+    "url": "/static/js/main.45c232a3.chunk.js"
+  },
+  {
+    "revision": "06bd6254f13ed7fc7150",
+    "url": "/static/js/runtime-main.74d2343d.js"
+  },
+  {
+    "revision": "e6e2e100d7ac0bfbb36f77834e2e9f99",
+    "url": "/static/media/adaptive.e6e2e100.png"
+  },
+  {
+    "revision": "1e4a7403976657db35642dd3acb230a0",
+    "url": "/static/media/ctrip.1e4a7403.png"
+  },
+  {
+    "revision": "bcbb11f59952711861841e828cba11fd",
+    "url": "/static/media/self.bcbb11f5.jpg"
+  }
+]);
